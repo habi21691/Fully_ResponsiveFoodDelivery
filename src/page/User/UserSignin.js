6c80ecffe4898ce7261ca6b3cf722feeb148e3ad
habi <habtemariamshumye46@gmail.com>
2022-11-07 00:53:00 +0300

    change

--- src/page/User/UserSignin.js
+++ src/page/User/UserSignin.js
@@ -175,7 +175,7 @@ const Alert = React.forwardRef(function Alert(props, ref) {
                 <TextField
                   required
                   fullWidth
-                  inputProps={{maxLength:14, minLength:6}}
+                  inputProps={{maxLength:14, minLength:4}}
                   variant="standard"
                   value={username}
                   label="User Name:"
